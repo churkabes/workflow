2aff27174dd5b127bbc2bd3c03efdc7519310883
churkabes <churkabes555@gmail.com>
2017-02-24 22:01:59 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,16 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MainWorker cMainWorker = new MainWorker();
+        cMainWorker.setHandler(new TestHandler());
+        cMainWorker.start();
+        //cMainWorker.stop();
+    }
+}
+
+class TestHandler implements IHandler {
+    @Override
+    public void onEvent(String name, String data) {
+        System.out.println("onEvent -> " +  name + ":" + data);
+    }
+}
 
